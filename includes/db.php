2f83c67942ecf9278f603156cccbbab709bb57a7
priyesh921 <priyeshagarwal50@gmail.com>
2025-03-08 11:52:58 +0530

    registration page

--- includes/db.php
+++ includes/db.php
@@ -1,11 +1,12 @@
 <?php
 $host = "localhost";
-$user = "root";  // Change if needed
-$pass = "";  // Change if needed
+$user = "root";  
+$pass = "";  
 $db = "blog_platform";
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+    
 }
 ?>
